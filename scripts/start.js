f13b28fa780622e20f982e1a7fb8c4271ac4300c
Vishwa Phansal <43176146+vishwaphansal7@users.noreply.github.com>
2021-09-06 10:25:02 +0530

    Update start.js

--- scripts/start.js
+++ scripts/start.js
@@ -1,17 +1,21 @@
 'use strict';
-try {
+try 
+{
   require('node-env-file')('.env');
 }
-catch (e) {
+catch (e) 
+{
   console.log(e);
 }
-finally {
+finally 
+{
   var merchantAPI = require('../index.js');
   merchantAPI.start({
     port: process.env.PORT || 3000,
     bind: process.env.BIND || '127.0.0.1'
   });
-  if (process.env.RPC) {
+  if (process.env.RPC) 
+  {
     merchantAPI.startRPC({
       api_code: process.env.API_CODE
     });
